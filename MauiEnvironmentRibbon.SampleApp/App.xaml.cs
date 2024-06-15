a2a7ae2e77bc7d564288bd63dce71af145ea477f
jasho <roman.jasek@hotmail.com>
2024-06-15 07:48:53 +0200

    Add default MAUI project

---
+++ MauiEnvironmentRibbon.SampleApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiEnvironmentRibbon.SampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
