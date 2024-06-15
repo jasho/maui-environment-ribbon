3e7506dd526eba1aac949a0576e63ddd6261b2a0
jasho <roman.jasek@hotmail.com>
2024-06-15 16:13:07 +0200

    Add support for top-level TabbedPage

---
+++ src/MauiEnvironmentRibbon/Extensions/TabbedPageExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiEnvironmentRibbon.Extensions;
+
+public static class TabbedPageExtensions
+{
+    public static TabbedPage AddEnvironmentRibbon(this TabbedPage tabbedPage, EnvironmentRibbonType environmentRibbonType)
+    {
+        EnvironmentRibbonService.SetConfiguration(environmentRibbonType);
+        foreach (var child in tabbedPage.Children)
+        {
+            EnvironmentRibbonService.AddEnvironmentRibbonToPage(child);
+        }
+
+        return tabbedPage;
+    }
+
+    public static TabbedPage AddEnvironmentRibbon(this TabbedPage tabbedPage, EnvironmentRibbonConfiguration configuration)
+    {
+        EnvironmentRibbonService.SetConfiguration(configuration);
+        foreach (var child in tabbedPage.Children)
+        {
+            EnvironmentRibbonService.AddEnvironmentRibbonToPage(child);
+        }
+
+        return tabbedPage;
+    }
+}
