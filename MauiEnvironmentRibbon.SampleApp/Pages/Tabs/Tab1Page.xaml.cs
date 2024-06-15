27dfbbd66c8d525dca269a05ce20ba96cc752ce9
jasho <roman.jasek@hotmail.com>
2024-06-15 11:15:32 +0200

    Add Flyout and TabBar to AppShell

---
+++ MauiEnvironmentRibbon.SampleApp/Pages/Tabs/Tab1Page.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiEnvironmentRibbon.SampleApp.Pages.Tabs;
+
+public partial class Tab1Page : ContentPage
+{
+	public Tab1Page()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiEnvironmentRibbon.SampleApp/Pages/Tabs/Tab2Page.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiEnvironmentRibbon.SampleApp.Pages.Tabs;
+
+public partial class Tab2Page : ContentPage
+{
+	public Tab2Page()
+	{
+		InitializeComponent();
+	}
+}
