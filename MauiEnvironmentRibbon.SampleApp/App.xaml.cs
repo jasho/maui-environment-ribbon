7d1d6f730f275c30d498d6ba1341014b519c1204
jasho <roman.jasek@hotmail.com>
2024-06-15 09:44:20 +0200

    Move code that adds the ribbon to ShellExtensions class

--- MauiEnvironmentRibbon.SampleApp/App.xaml.cs
+++ MauiEnvironmentRibbon.SampleApp/App.xaml.cs
@@ -1,45 +1,14 @@
-﻿using MauiEnvironmentRibbon.SampleApp.Controls;
-
-namespace MauiEnvironmentRibbon.SampleApp
+﻿namespace MauiEnvironmentRibbon.SampleApp
 {
-    public partial class App : Application
+    public partial class App
     {
         public App()
         {
             InitializeComponent();
 
-            var appShell = new AppShell();
-            appShell.Navigated += AppShell_Navigated;
-
+            var appShell = new AppShell()
+                .AddEnvironmentRibbon();
             MainPage = appShell;
-        }
-
-        private void AppShell_Navigated(object? sender, ShellNavigatedEventArgs e)
-        {
-            if (sender is Shell shell)
-            {
-                AddEnvironmentRibbonToShell(shell);
-            }
-        }
-
-        private void AddEnvironmentRibbonToShell(Shell shell)
-        {
-            if (shell.CurrentPage is ContentPage contentPage)
-            {
-                if (contentPage.Content is Grid rootGrid)
-                {
-                    rootGrid.Children.Add(new EnvironmentRibbon());
-                }
-                else
-                {
-                    var newRootGrid = new Grid();
-
-                    newRootGrid.Children.Add(contentPage.Content);
-                    newRootGrid.Children.Add(new EnvironmentRibbon());
-
-                    contentPage.Content = newRootGrid;
-                }
-            }
         }
     }
 }
---
+++ MauiEnvironmentRibbon.SampleApp/ShellExtensions.cs
@@ -0,0 +1,40 @@
+﻿using MauiEnvironmentRibbon.SampleApp.Controls;
+
+namespace MauiEnvironmentRibbon.SampleApp;
+
+public static class ShellExtensions
+{
+    public static Shell AddEnvironmentRibbon(this Shell shell)
+    {
+        shell.Navigated += AppShell_Navigated;
+        return shell;
+    }
+
+    private static void AppShell_Navigated(object? sender, ShellNavigatedEventArgs e)
+    {
+        if (sender is Shell { CurrentPage: not null } shell)
+        {
+            AddEnvironmentRibbonToPage(shell.CurrentPage);
+        }
+    }
+
+    private static void AddEnvironmentRibbonToPage(Page page)
+    {
+        if (page is ContentPage contentPage)
+        {
+            if (contentPage.Content is Grid rootGrid)
+            {
+                rootGrid.Children.Add(new EnvironmentRibbon());
+            }
+            else
+            {
+                var newRootGrid = new Grid();
+
+                newRootGrid.Children.Add(contentPage.Content);
+                newRootGrid.Children.Add(new EnvironmentRibbon());
+
+                contentPage.Content = newRootGrid;
+            }
+        }
+    }
+}
